a7e35676a15d8513459f4da90ed6f2dd3ec4ca74
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-09-07 12:47:01 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, {Component} from "react";
 
-class App extends React.Component {
+class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello, world</h1>
-      </div>
-    );
+      <>
+        <h1>hello</h1>
+      </>
+    )
   }
 
 }
