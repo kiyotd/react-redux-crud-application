6b22b62c7bb9fe4b7d9ffe67fe9861d25cb2aede
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-09-07 13:14:49 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, {Component} from "react";
+import React from "react";
 
-class App extends Component {
+// class App extends Component {
+//
+//   render() {
+//     return (
+//       <>
+//         <h1>hello</h1>
+//       </>
+//     )
+//   }
+//
+// }
 
-  render() {
-    return (
-      <>
-        <h1>hello</h1>
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <>
+      <Cat/>
+    </>
+  );
+};
 
-}
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
