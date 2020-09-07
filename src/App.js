76735e9957a3e7b018ee82151cc491bf3f3174ee
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-09-07 12:29:08 +0900

    関数コンポーネントからクラスコンポーネントに変更

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world</h1>
+      </div>
+    );
+  }
+
 }
 
 export default App;
